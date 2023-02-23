eb58d378702c72a5b0e26418c803088694bc0ba8
dogoLucas <lucasyeliana2018@gmail.com>
2023-02-22 21:23:05 -0300

    añadido el controlador para que sean posibles los protocolos

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const config = require("./src/config/config.json");
+const personaCont = require("./src/controller/personaController.js");
+app.use("/api/persona",personaCont);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,17 +13,17 @@ morgan(':method :url :status :res[content-length] - :response-time ms');
   
 
 
-app.get("/", function (req, res) {
+/* app.get("/", function (req, res) {
     res.send("ALPHA-SILICON");
-});
+}); */
 
-const personaCont = require("./src/controller/personaController.js");
-app.use("/api/persona",personaCont);
+
 
 /*
 const userCont = require("userController.js");
 app.use("/api/usuario",userCont);
 */
+app.use(require('./src/controller/personaController.js'))
 
 app.listen(config.server.port, function (err) {
     if (err) {
